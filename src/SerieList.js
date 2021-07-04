b6b4082c6ae8567c2cef25895af05cfe40c04ef9
Laurent Kévin <kevin-laurent@hotmail.fr>
2021-07-04 20:38:19 +0200

    ajout

--- src/SerieList.js
+++ src/SerieList.js
@@ -4,9 +4,10 @@ import './SerieList.css'
 
 export default function SerieList(props) {
     const elements = props.series.map(serie => <Serie key={serie.id} serie={serie} />)
-
+    
     return (
         <div classNames="container">
-        {elements}</div>
+            
+        </div>
     )
 }
